35d17650e48bb861b805023362b52fcdab39736a
popomore <sakura9515@gmail.com>
2014-04-01 00:39:47 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = imports;
+
+function imports(str, fn) {
+  if (str instanceof Buffer) str = str.toString();
+  if (fn) return map(str, fn);
+  var re = /@import *url\(['"]([^'"]+)['"]\);/g;
+  var ret = [];
+  var m;
+
+  while (m = re.exec(str)) {
+    ret.push({
+      string: m[0],
+      path: m[1],
+      index: m.index
+    });
+  }
+
+  return ret;
+}
+
+function map(str, fn) {
+  imports(str).forEach(function(r){
+    str = str.replace(r.string, fn(r));
+  });
+
+  return str;
+}
---
+++ test/index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+require('should');
+var fs = require('fs');
+var less = require('less');
+var imports = require('..');
+
+describe('css-imports', function() {
+
+  it('return all @import', function() {
+    var code = fs.readFileSync(__dirname + '/fixtures/normal.css');
+    var ret = imports(code);
+    ret.should.eql([
+      {
+        string: '@import url(\'./b.css\');',
+        path: './b.css',
+        index: 0
+      },
+      {
+        string: '@import url("c");',
+        path: 'c',
+        index: 50
+      }
+    ]);
+  });
+
+  it('replace @import', function() {
+    var code = fs.readFileSync(__dirname + '/fixtures/normal.css');
+    var ret = imports(code, normalize);
+
+    ret.should.include('@import url("./b.css");');
+    ret.should.include('@import url("c");');
+
+    function normalize(item) {
+      return '@import url("' + item.path + '");';
+    }
+  });
+
+  it('less example', function(done) {
+    var code = fs.readFileSync(__dirname + '/fixtures/normal.less').toString();
+
+    parseLess(code, function(err, ret) {
+      if (err) return done(err);
+
+      imports(ret).should.eql([{
+        string: '@import url("bar.css");',
+        path: 'bar.css',
+        index: 0
+      }]);
+      done();
+    });
+  });
+});
+
+function parseLess (code, cb) {
+  var parser = new(less.Parser)({
+    paths: [__dirname + '/fixtures']
+  });
+  parser.parse(code, function (err, tree) {
+    console.log(err)
+    cb(err, tree.toCSS());
+  });
+}
